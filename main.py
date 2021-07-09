07996b18132eac91bf14e321bf10b90dc92b312d
princecodes247 <princecodes247@gmail.com>
2021-07-09 09:59:58 -0400

    feat: update

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import base64
 from github import Github
 from pprint import pprint
+from datetime import datetime
+from time import sleep
+import os
 
-# Github username and password
-username = "username"
-password = "password"
+# Github access token
+access_token = os.getenv('GITHUB_TOKEN')
 
 # authenticate to github
-g = Github(username, password)
+g = Github(access_token)
 # get the authenticated user
 user = g.get_user()
-for repo in user.get_repos():
-    print_repo(repo)
+
+def commit_it(target): 
+    repo = g.get_repo(target)
+    branch = repo.get_branch("main")
+    ref = branch.commit.sha
+    # Current date time in local system
+    commit = datetime.now()
+    contents = repo.get_contents("commit.txt", ref=ref)
+    repo.update_file(contents.path, f"{commit}", f"{commit}", contents.sha, branch="main")
+    print("Made Commit")
+while True:
+    commit_it("princecodes247/pyCommited")
+    sleep(10)
